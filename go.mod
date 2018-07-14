b7dc514ccdc403f9d0c5abe6bfd4139f71c11bfd
jdlau <jdlau@qq.com>
2018-07-14 21:54:32 +0800

    go module test case

---
+++ go.mod
@@ -0,0 +1,3 @@
+module jdlau.com/module
+
+replace jdlau.com/module/a v1.0.0 => ./a
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"jdlau.com/module/a"
+)
+
+func main() {
+	fmt.Println(a.A)
+	fmt.Println("hello")
+}
